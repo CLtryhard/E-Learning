fa6a0d12046685aa8373f05f3bcc49a8a4fefb6d
Lance <cltryhard@163.com>
2019-01-12 18:34:45 +0800

    提交项目的基本骨架

---
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -0,0 +1,28 @@
+package com.xuecheng.framework.domain.cms.request;
+
+import com.xuecheng.framework.model.request.RequestData;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Author: Lacne
+ * @Date: 2019/1/12 21:51
+ */
+@Data
+public class QueryPageRequest extends RequestData {
+    //站点id
+    @ApiModelProperty("站点id")
+    private String siteId;
+    //页面ID
+    @ApiModelProperty("页面id")
+    private String pageId;
+    //页面名称
+    @ApiModelProperty("页面名称")
+    private String pageName;
+    //别名
+    @ApiModelProperty("别名")
+    private String pageAliase;
+    //模版id
+    @ApiModelProperty("模板id")
+    private String templateId;
+}
