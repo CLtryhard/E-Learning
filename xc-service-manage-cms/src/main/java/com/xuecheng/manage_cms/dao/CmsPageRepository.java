3ebe562fc9417816bc2b7d75788582644dcb027a
Lance <cltryhard@163.com>
2019-01-14 21:53:30 +0800

    将抽象方法的注释改为了javadoc注释

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
@@ -10,12 +10,35 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * @Date: 2019/1/14 10:23
  */
 public interface CmsPageRepository extends MongoRepository<CmsPage,String> {
-    //根据页面名称查询
+    /**
+     * 根据页面名称查询
+     * @param pageName
+     * @return
+     */
     CmsPage findByPageName(String pageName);
-    //根据页面名称和类型查询
+
+    /**
+     * 根据页面名称和类型查询
+     * @param pageName
+     * @param pageType
+     * @return
+     */
     CmsPage findByPageNameAndPageType(String pageName,String pageType);
-    //根据站点和页面类型查询记录数
+
+    /**
+     * 根据站点和页面类型查询记录数
+     * @param siteId
+     * @param pageType
+     * @return
+     */
     int countBySiteIdAndPageType(String siteId,String pageType);
-    //根据站点和页面类型分页查询
+
+    /**
+     * 根据站点和页面类型分页查询
+     * @param siteId
+     * @param pageType
+     * @param pageable
+     * @return
+     */
     Page<CmsPage> findBySiteIdAndPageType(String siteId, String pageType, Pageable pageable);
 }
